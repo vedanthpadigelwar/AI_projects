5520eb5ef2a1347fa108ea75efc9c5cb989370e1
vedanthpadigelwar <vedanthpadigelwar@gmail.com>
2021-08-29 14:59:57 +0530

    Update demo.py

--- person_tracking_with_yolov4_and_deepsort/tensorflow2.0/deep-sort-yolov4/demo.py
+++ person_tracking_with_yolov4_and_deepsort/tensorflow2.0/deep-sort-yolov4/demo.py
@@ -21,18 +21,20 @@ from videocaptureasync import VideoCaptureAsync
 
 warnings.filterwarnings('ignore')
 
+
 def main(yolo):
 
     # Definition of the parameters
     max_cosine_distance = 0.3
     nn_budget = None
     nms_max_overlap = 1.0
-    
+
     # Deep SORT
     model_filename = 'model_data/mars-small128.pb'
     encoder = gdet.create_box_encoder(model_filename, batch_size=1)
-    
-    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+
+    metric = nn_matching.NearestNeighborDistanceMetric(
+        "cosine", max_cosine_distance, nn_budget)
     tracker = Tracker(metric)
 
     tracking = True
@@ -40,7 +42,7 @@ def main(yolo):
     asyncVideo_flag = False
 
     file_path = 'video.webm'
-    if asyncVideo_flag :
+    if asyncVideo_flag:
         video_capture = VideoCaptureAsync(file_path)
     else:
         video_capture = cv2.VideoCapture(file_path)
@@ -65,11 +67,11 @@ def main(yolo):
     while True:
         ret, frame = video_capture.read()  # frame shape 640*480*3
         if ret != True:
-             break
+            break
 
         t1 = time.time()
 
-        image = Image.fromarray(frame[...,::-1])  # bgr to rgb
+        image = Image.fromarray(frame[..., ::-1])  # bgr to rgb
         boxes, confidence, classes = yolo.detect_image(image)
 
         if tracking:
@@ -84,7 +86,8 @@ def main(yolo):
         # Run non-maxima suppression.
         boxes = np.array([d.tlwh for d in detections])
         scores = np.array([d.confidence for d in detections])
-        indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
+        indices = preprocessing.non_max_suppression(
+            boxes, nms_max_overlap, scores)
         detections = [detections[i] for i in indices]
 
         if tracking:
@@ -96,22 +99,24 @@ def main(yolo):
                 if not track.is_confirmed() or track.time_since_update > 1:
                     continue
                 bbox = track.to_tlbr()
-                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 2)
+                cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(
+                    bbox[2]), int(bbox[3])), (255, 255, 255), 2)
                 cv2.putText(frame, "ID: " + str(track.track_id), (int(bbox[0]), int(bbox[1])), 0,
                             1.5e-3 * frame.shape[0], (0, 255, 0), 1)
 
         for det in detections:
             bbox = det.to_tlbr()
             score = "%.2f" % round(det.confidence * 100, 2) + "%"
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), 2)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(
+                bbox[2]), int(bbox[3])), (255, 0, 0), 2)
             if len(classes) > 0:
                 cls = det.cls
                 cv2.putText(frame, str(cls) + " " + score, (int(bbox[0]), int(bbox[3])), 0,
                             1.5e-3 * frame.shape[0], (0, 255, 0), 1)
 
-        cv2.imshow('', frame)
+        #cv2.imshow('', frame)
 
-        if writeVideo_flag: # and not asyncVideo_flag:
+        if writeVideo_flag:  # and not asyncVideo_flag:
             # save a frame
             out.write(frame)
             frame_index = frame_index + 1
@@ -120,8 +125,8 @@ def main(yolo):
 
         if not asyncVideo_flag:
             fps = (fps + (1./(time.time()-t1))) / 2
-            print("FPS = %f"%(fps))
-        
+            print("FPS = %f" % (fps))
+
         # Press Q to stop!
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -137,7 +142,8 @@ def main(yolo):
     if writeVideo_flag:
         out.release()
 
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
     main(YOLO())
